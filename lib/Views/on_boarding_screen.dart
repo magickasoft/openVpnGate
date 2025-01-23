461d6d5e9e9167ae4ae5b07da6c31dd471c046ce
AtrVorobev <artvorobev96@gmail.com>
2025-01-23 23:48:25 +0400

    add new

--- lib/Views/on_boarding_screen.dart
+++ lib/Views/on_boarding_screen.dart
@@ -13,6 +13,15 @@ class OnBoardingScreen extends StatefulWidget {
 }
 
 class _OnBoardingScreen extends State<OnBoardingScreen> {
+  int currentIndex = 0;
+  PageController? _pageController;
+
+  @override
+  void initState() {
+    _pageController = PageController(initialPage: 0);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,31 +31,47 @@ class _OnBoardingScreen extends State<OnBoardingScreen> {
           Expanded(
             child: PageView.builder(
               itemCount: contents.length,
+              controller: _pageController,
+              onPageChanged: (int index) {
+                if(index > 0) {
+                  setState(() {
+                    currentIndex = index;
+                  });
+                } else {
+                    return null;
+                  }
+              },
               itemBuilder: (context, index) {
                 return Padding(padding: const EdgeInsets.all(30.0),
                   child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    SvgPicture.asset(
-                      contents[index].image,
-                      fit: BoxFit.fill,
-                      placeholderBuilder: (context) => CircularProgressIndicator(),
-                      // Добавьте обработку ошибок
-                      errorBuilder: (context, error, stackTrace) {
-                        print("Error loading image: ${contents[index].image}");
-                        return Text("Error loading image");
-                      },
+                    Expanded(
+                      flex: 4,
+                      child: SvgPicture.asset(
+                        contents[index].image,
+                        fit: BoxFit.fill,
+                      ),
                     ),
-                    Text(
-                      contents[index].title,
-                      style: boldStyle,
-                      textAlign: TextAlign.center,
+                    Expanded(
+                      flex: 1,
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 25.0),
+                        child: Text(
+                          contents[index].title,
+                          style: boldStyle,
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
                     ),
-                    Text(
-                      contents[index].title,
-                      style: mediumStyle,
-                      textAlign: TextAlign.center,
-                    )
+                    Expanded(
+                      flex: 2,
+                      child: Text(
+                        contents[index].description,
+                        style: mediumStyle,
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
                   ],
                 )
                 );
@@ -56,5 +81,10 @@ class _OnBoardingScreen extends State<OnBoardingScreen> {
         ],
       ),
     );
+  }
+  @override
+  void dispose() {
+    _pageController!.dispose();
+    super.dispose();
   }
 }
